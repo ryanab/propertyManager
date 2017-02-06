b842d1f8824745586d530f2b0c1acb5979547515
ryanab <ryanabyrne@gmail.com>
2017-02-06 22:05:32 +0000

    Created post and get actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,38 @@
 import constants from '../constants'
+import { APIManager } from '../APIManager'
 
+    const getRequest = (path, params, actionType)=>{
+      return (dispatch) => {
+        APIManager.get(path, params)
+        .then(response=>{
+          const payload = response.data || response.user
+          dispatch({
+            type: actionType,
+            payload: payload
+          })
+        })
+        .catch(err=>{
+          throw err        
+        })
+      }
+    }
+    
+    const postRequest = (path, params, actionType)=>{
+      return (dispatch) => {
+        APIManager.post(path, params)
+        .then(response=>{
+          const payload = response.data || response.user
+          dispatch({
+            type: actionType,
+            payload: payload
+          })
+        })
+        .catch(err=>{
+          throw err        
+        })
+      }
+    }
+
+export default {
+    
+}
