217c9ea69415f39293473450558f3151f59e3cc4
Ryan Byrne <ryanbyrne@Ryans-MacBook-Pro.local>
2017-02-20 08:25:07 -0500

    merge conflicts

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import { Home } from './components/layout'
+import { Home, Availabilities } from './components/layout'
 import { Provider } from 'react-redux'
 import store from './stores'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router' 
 
 const app = (
-    <Provider store={ store.configureStore() }>
-        <div>
-            <Home />
-        </div>
-    </Provider >
+  <Provider store={ store.configureStore() }>
+    <Router history={browserHistory} >
+      <Route path="/" component={Home} />
+      <Route path="/:company/availabilities" component={Availabilities} />
+    </Router>
+  </Provider >
 )
 
 ReactDOM.render(app, document.getElementById('root'))
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,5 +1,7 @@
 import Home from './Home'
+import Availabilities from './Availabilities'
 
 export {
-    Home
+    Home,
+    Availabilities
 }
